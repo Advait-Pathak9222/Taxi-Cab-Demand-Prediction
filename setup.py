fa74a395dda45ce612a7b9acc757779fa67d88f3
Advait-Pathak9222 <advaitpathak12@gmail.com>
2025-06-12 10:35:13 +0530

    Initial Cookiecutter Template Initialization for the Project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Machine Learning Project that helps Cab drivers to navigateto places of high cab demand which helps in maximizing the money they make in a day along with maximizing the cab company profits.',
+    author='Advait Pathak',
+    license='',
+)
